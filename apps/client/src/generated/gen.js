ccdaf0d074d1114a9c55845215ca0142fa523b25
Ethan Stafford <ethanstafford.dev@gmail.com>
2026-03-10 08:34:58 -0700

    new version

--- apps/client/src/generated/gen.js
+++ apps/client/src/generated/gen.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () =>
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1772395785282");
+		needleEngine.setAttribute("hash", "1773089529530");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
