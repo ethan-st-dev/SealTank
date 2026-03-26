ae37c495091d01da1f3d257ad928d573ae10040b
Ethan Stafford <ethanstafford.dev@gmail.com>
2026-03-25 18:17:44 -0700

    change to easygrab

--- apps/client/src/generated/gen.js
+++ apps/client/src/generated/gen.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () =>
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1774386458384");
+		needleEngine.setAttribute("hash", "1774484858815");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
--- apps/client/src/scripts/EasyGrab.ts
+++ apps/client/src/scripts/EasyGrab.ts
@@ -7,18 +7,31 @@ export class EasyGrab extends Behaviour implements IPointerEventHandler {
 
     start() {
         this.rb = this.gameObject.getComponent(Rigidbody);
+        if (!this.rb) {
+            console.warn("EasyGrab: No Rigidbody found on", this.gameObject.name);
+        } else {
+            console.log("EasyGrab: Found Rigidbody on", this.gameObject.name, "isKinematic:", this.rb.isKinematic);
+        }
     }
 
     onPointerDown(_pd: PointerEventData) {
+        console.log("EasyGrab: onPointerDown", this.gameObject.name);
         if (this.rb) {
-            this.rb.isKinematic = true; 
+            console.log("EasyGrab: Setting kinematic=true, was:", this.rb.isKinematic);
+            this.rb.isKinematic = true;
+            this.rb.useGravity = false;
             this.rb.setVelocity(0, 0, 0);
+            this.rb.setAngularVelocity(0, 0, 0);
+            console.log("EasyGrab: After setting - kinematic:", this.rb.isKinematic, "useGravity:", this.rb.useGravity);
         }
     }
 
     onPointerUp(_pd: PointerEventData) {
+        console.log("EasyGrab: onPointerUp", this.gameObject.name);
         if (this.rb) {
             this.rb.isKinematic = false;
+            this.rb.useGravity = true;
+            console.log("EasyGrab: Released - kinematic:", this.rb.isKinematic, "useGravity:", this.rb.useGravity);
         }
     }
 }
